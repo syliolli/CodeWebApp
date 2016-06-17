9a33e3567188b90914013c69c11d2d5edc77b897
Sampo Yli-Olli <sampo.yli-olli@iki.fi>
2016-06-17 23:18:11 +0300

    Second commit

--- server.js
+++ server.js
@@ -3,4 +3,5 @@ http.createServer(function (req, res) {
     console.log('Got request for ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('<h1>Hello Code and Azure Web Apps!</h1>');
-}).listen(process.env.PORT);
+    
+}).listen(process.env.PORT); 
