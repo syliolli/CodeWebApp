35a38deb95e3b6ad614fa51e430ff0e1a02efd44
Sampo Yli-Olli <sampo.yli-olli@iki.fi>
2016-06-17 23:21:20 +0300

    Commit with test change

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log('Got request for ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello Code and Azure Web Apps!</h1>');
-    
+    res.end('<h1>Hello Code and Azure Web Apps by Sampo!</h1>');
+
 }).listen(process.env.PORT); 
